e966b5fbdbbdd4a7927d39583cf66a499d257827
jsieber2 <josefwsieber@gmail.com>
2023-07-09 16:12:49 -0400

    header made

---
+++ turtle-portfolio/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* More components will go here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ turtle-portfolio/src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h1> Data Science / ML Engineering / Data Engineering</h1>
+            <nav>
+                <ul>
+                    <li> <a href="#about" >About</a></li>
+                    <li> <a href="#dataScience">Data Science</a></li>
+                    <li> <a href="#mlEngineering">ML Engineering</a></li>
+                    <li> <a href="#dataEngineering"> Data Engineering</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header;
