f28448dfa6019e798fafb095dd9655aed6fc43eb
jsieber2 <josefwsieber@gmail.com>
2023-07-09 22:33:12 -0400

    dockerized

---
+++ turtle-portfolio/src/About.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from './About';
+
+test('renders the correct content', () => {
+    render(<About />);
+    const aboutElement = screen.getByText(/about me/i);
+    expect(aboutElement).toBeInTheDocument();
+
+});
